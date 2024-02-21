7033ffce9b4fb008230e0bcb20586fed576d4bb9
Your Name <you@example.com>
2024-02-21 14:18:11 -0300

    adcionando projeto

---
+++ index.js
@@ -0,0 +1,59 @@
+const { Sequelize, DataTypes} = require('sequelize');
+
+const sequelize = new Sequelize('bd_2', 'postgres', 'postgres', {
+    host: 'localhost',
+    dialect:'postgres'
+});
+conectar= async ()=>{
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+}
+conectar();
+//funcao para criar uma tabela no banco
+const Usuario = sequelize.define('Usuario', {
+  // Model attributes are defined here
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    primaryKey: true
+  },
+  nascimento:{
+    type:DataTypes.DATE
+  }
+}, {
+  // Other model options go here
+});
+console.log(Usuario === sequelize.models.Usuario); // true
+//funcao usada para sincronizar como banco
+async function sincronizar(){
+  await Usuario.sync();
+  console.log("usuario sincronizado");
+  }
+  sincronizar();
+  
+  //funcao usada para criar um usuario
+  async function criarUsuario(usuario){
+    await Usuario.create(usuario);
+    console.log("usuario criado");
+  }
+  
+  //criarUsuario({
+  //  nome:"lucio",
+  //  email:"lucio@gmail.com",
+  //  nascimento:"2003-11-22"
+  //})
+
+  //funcao para listar as tabelas
+async function listarUsuarios(){
+  const usuarios =await Usuario.findAll();
+  console.log(JSON.stringify(usuarios));
+}
+
+listarUsuarios();
